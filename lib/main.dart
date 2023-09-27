5553cc1fe3330e0e4adb0a8d389e5576c0c96aaf
Hishamk99 <hishamkamel370@gmail.com>
2023-09-27 21:29:46 +0200

    adding phone and email

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,12 @@ class BusinessCardApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xff324E6B),
+        backgroundColor: const Color(0xff324E6B),
         body: Column(
           children: [
-            CircleAvatar(
+            const CircleAvatar(
               backgroundColor: Colors.white,
               radius: 122,
               child: CircleAvatar(
@@ -22,12 +22,69 @@ class BusinessCardApp extends StatelessWidget {
                 backgroundImage: AssetImage('assets/images/birth.png'),
               ),
             ),
-            Text(
+            const Text(
               'Hisham Kamel',
               style: TextStyle(
                 fontFamily: 'Pacifico',
                 fontSize: 30,
                 color: Colors.white,
+              ),
+            ),
+            const Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                  fontSize: 16,
+                  color: Color(0xff6c8090),
+                  fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16 ,vertical: 8),
+              child: Container(
+                height: 65,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8)),
+                child: const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: Icon(
+                        Icons.phone,
+                        size: 32,
+                        color: Color(0xff324E6B),
+                      ),
+                    ),
+                    Text(
+                      '(+20)1010861564',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16 ,vertical: 8),
+              child: Container(
+                height: 65,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8)),
+                child: const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: Icon(
+                        Icons.mail,
+                        size: 32,
+                        color: Color(0xff324E6B),
+                      ),
+                    ),
+                    Text(
+                      'hisham370@gmail.com',
+                      style: TextStyle(fontSize: 24),
+                    )
+                  ],
+                ),
               ),
             )
           ],
