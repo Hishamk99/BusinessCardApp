688fa69676af04c1267b84d380c5c76af2d6113e
Hishamk99 <hishamkamel370@gmail.com>
2023-09-28 11:51:34 +0200

    using ListTile widget instead of container

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,14 @@ class BusinessCardApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: const Color(0xff324E6B),
+        backgroundColor: Color(0xff324E6B),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const CircleAvatar(
+            CircleAvatar(
               backgroundColor: Colors.white,
               radius: 112,
               child: CircleAvatar(
@@ -23,7 +24,7 @@ class BusinessCardApp extends StatelessWidget {
                 backgroundImage: AssetImage('assets/images/birth.png'),
               ),
             ),
-            const Text(
+            Text(
               'Hisham Kamel',
               style: TextStyle(
                 fontFamily: 'Pacifico',
@@ -31,76 +32,48 @@ class BusinessCardApp extends StatelessWidget {
                 color: Colors.white,
               ),
             ),
-            const Text(
+            Text(
               'FLUTTER DEVELOPER',
               style: TextStyle(
                   fontSize: 16,
                   color: Color(0xff6c8090),
                   fontWeight: FontWeight.bold),
             ),
-            const Divider(
+            Divider(
               color: Color(0xff6c8090),
               thickness: 1,
               indent: 60,
               endIndent: 60,
               height: 10,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Container(
-                height: 65,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8)),
-                child: const Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 16),
-                      child: Icon(
-                        Icons.phone,
-                        size: 32,
-                        color: Color(0xff324E6B),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 16),
-                      child: Text(
-                        '(+20)1010861564',
-                        style: TextStyle(fontSize: 24),
-                      ),
-                    ),
-                  ],
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 16 , vertical: 8),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  size: 32,
+                  color: Color(0xff324E6B),
+                ),
+                title: Text(
+                  '(+20)1010861564',
+                  style: TextStyle(fontSize: 24),
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Container(
-                height: 65,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8)),
-                child: const Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 16),
-                      child: Icon(
-                        Icons.mail,
-                        size: 32,
-                        color: Color(0xff324E6B),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 16),
-                      child: Text(
-                        'hisham370@gmail.com',
-                        style: TextStyle(fontSize: 24),
-                      ),
-                    )
-                  ],
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 16 , vertical: 8),
+              child: ListTile(
+                leading: Icon(
+                  Icons.mail,
+                  size: 32,
+                  color: Color(0xff324E6B),
+                ),
+                title: Text(
+                  'hisham370@gmail.com',
+                  style: TextStyle(fontSize: 24),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
